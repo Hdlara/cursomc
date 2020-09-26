a3eab2c7eca4f840c82a1475bc985f5ec998141d
Henrique Dias <henriquediaslara@hotmail.com>
2020-09-26 11:02:33 -0300

    adicionando a possibilidade de ver todas as categorias sem os produtos atrelados

--- src/main/java/com/example/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e){
 			throw new DateIntegrityException("Não é possivel excluir uma categoria que tem produtos atraldos");
 		}
+	}
+	
+	public List<Categoria> findall(){
+		return repo.findAll();
 	}
 }
